cc2c6176aa0ae073b7e341635694e2961a7cd340
Anteneh Tilaye <antenehtilayekebede@gmail.com>
2024-06-07 15:39:03 -0700

    chat app

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,16 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
-  //instance of auth
+  //instance of auth and firestore
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  //get current user
+  User? getCurrentUser() {
+    return _auth.currentUser;
+  }
 
   // sign in
-
   Future<UserCredential> signInWithEmailPassword(String email, password) async {
     try {
+      // sign user in
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
         email: email,
         password: password,
+      );
+
+      //save user info if it doesn't exist
+      _firestore.collection("Users").doc(userCredential.user!.uid).set(
+        {
+          'uid': userCredential.user!.uid,
+          'email': email,
+        },
       );
       return userCredential;
     } on FirebaseAuthException catch (e) {
@@ -22,8 +37,21 @@ class AuthService {
 
   Future<UserCredential> signUpWithEmailPassword(String email, password) async {
     try {
-      UserCredential userCredential = await _auth
-          .createUserWithEmailAndPassword(email: email, password: password);
+      //create user
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      //save user info in separate logic
+      _firestore.collection("Users").doc(userCredential.user!.uid).set(
+        {
+          'uid': userCredential.user!.uid,
+          'email': email,
+        },
+      );
+
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
